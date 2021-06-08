6944936b4629916d3e41fe6e97d9100308a97e5b
github username <github email address>
2021-06-08 12:45:58 -0400

    commit tree 43274a8b27220d31b28d9629e3ef95f77843e402

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(arrObject) {
+    let winningYear = arrObject.find(win => {
+        if (win.result === 'W') {
+            return win.year
+        } else {
+            return undefined;
+        }
+    })
+    return !!winningYear ? winningYear.year : undefined
+}
